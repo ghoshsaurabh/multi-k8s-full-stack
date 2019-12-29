3fbcc019aae462895fa2c8efbeb2b0b269fc5f8e
Saurabh Ghosh <saurabh.ghosh83@gmail.com>
2019-12-29 13:57:52 -0500

    files not getting pushed to docker: try 2

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,13 @@ docker build -t ghoshsaurabh/multi-client:latest -t ghoshsaurabh/multi-client:$S
 docker build -t ghoshsaurabh/multi-server:latest -t ghoshsaurabh/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t ghoshsaurabh/multi-worker:latest -t ghoshsaurabh/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push ghoshsaurabh/mult-client:latest
-docker push ghoshsaurabh/mult-server:latest
-docker push ghoshsaurabh/mult-worker:latest
+docker push ghoshsaurabh/multi-client:latest
+docker push ghoshsaurabh/multi-server:latest
+docker push ghoshsaurabh/multi-worker:latest
 
-docker push ghoshsaurabh/mult-client:$SHA
-docker push ghoshsaurabh/mult-server:$SHA
-docker push ghoshsaurabh/mult-worker:$SHA
+docker push ghoshsaurabh/multi-client:$SHA
+docker push ghoshsaurabh/multi-server:$SHA
+docker push ghoshsaurabh/multi-worker:$SHA
 
 kubectl apply -f k8s
 
